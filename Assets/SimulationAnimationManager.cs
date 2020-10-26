f5cd6248a8f724d8aafa84a2af06db22a29aa6ca
Vookay <denijsbartmanis2@gmail.com>
2020-10-26 21:21:12 +0200

    Commit

---
+++ Assets/SimulationAnimationManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
+using System.Globalization;
+using System.Reflection;
+
+public class SimulationAnimationManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SimulationManager simulationManager;
+    public UnityEngine.UI.Image recycledImage;
+    public RectTransform recycledScale;
+    public UnityEngine.UI.Image recyclableImage;
+    public RectTransform recyclableScale;
+    public float percentage;
+    public float time;
+    public float maxRunTime;
+    private float timerTime;
+    private float runTimer;
+    internal float yearTime;
+    private float currentYear;
+    public TMP_Text timerText;
+    public TMP_Text yearText;
+    private float x, y;
+    private Vector2 baseScale;
+    public TMP_Text unrecycledWasteText;
+    private float unrecycledWaste;
+    public TMP_Text recycledWasteText;
+    private float recycledWaste;
+    private float waste;
+    public TMP_Text wasteText;
+    private float recyclableWaste;
+    public TMP_Text recyclableWasteText;
+    public bool running { get; set; }
+    private RectTransform unrecycledScale;
+    public GameObject blockingPanel;
+
+    void Start()
+    {
+        running = false;
+        timerTime = time;
+        runTimer = maxRunTime;
+        currentYear = 1;
+        x = 0f;
+        y = 0f;
+        baseScale = gameObject.GetComponent<RectTransform>().localScale;
+        gameObject.GetComponent<RectTransform>().localScale = new Vector2(0, 0);
+        unrecycledScale = gameObject.GetComponent<RectTransform>();
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running)
+        {
+            yearText.text = "Year " + currentYear;
+            yearTime -= Time.deltaTime;
+            if (yearTime <= 0)
+            {
+                currentYear++;
+                yearTime = maxRunTime / simulationManager.years;
+            }
+            runTimer -= Time.deltaTime;
+            if (runTimer <= 0)
+            {
+                running = false;
+                unrecycledWasteText.text = simulationManager.unrecycledWaste.ToString("##,#", CultureInfo.CurrentCulture) + " t";
+                recycledWasteText.text = simulationManager.recycledWaste.ToString("##,#", CultureInfo.CurrentCulture) + " t";
+                wasteText.text = simulationManager.waste.ToString("##,#", CultureInfo.CurrentCulture) + " t";
+                recyclableWasteText.text = simulationManager.recyclableWaste.ToString("##,#", CultureInfo.CurrentCulture) + " t";
+                blockingPanel.SetActive(true);
+                return;
+            }       
+            if (timerTime <= 0)
+            {
+                timerTime = time;
+                unrecycledScale.localScale = new Vector2(x, y);
+                if (unrecycledScale.localScale.x < baseScale.x)
+                    x += baseScale.x/(maxRunTime/time);
+                if (unrecycledScale.localScale.y < baseScale.y)
+                    y += baseScale.y / (maxRunTime / time);
+                unrecycledWaste += simulationManager.unrecycledWaste / (maxRunTime / time);
+                recycledWaste +=  simulationManager.recycledWaste / (maxRunTime / time);
+                recyclableWaste += simulationManager.recyclableWaste / (maxRunTime / time);
+                waste += simulationManager.waste / (maxRunTime / time);
+                unrecycledWasteText.text = Mathf.Round(unrecycledWaste).ToString("##,#", CultureInfo.CurrentCulture) + " t";
+                recycledWasteText.text = Mathf.Round(recycledWaste).ToString("##,#", CultureInfo.CurrentCulture) + " t";
+                recyclableWasteText.text = Mathf.Round(recyclableWaste).ToString("##,#", CultureInfo.CurrentCulture) + " t";
+                wasteText.text = Mathf.Round(waste).ToString("##,#", CultureInfo.CurrentCulture) + " t";
+                recycledScale.localScale = new Vector2(unrecycledScale.localScale.x, unrecycledScale.localScale.y);
+                recycledImage.fillAmount = recycledWaste / waste;
+                recyclableScale.localScale = new Vector2(unrecycledScale.localScale.x, unrecycledScale.localScale.y);
+                recyclableImage.fillAmount = recycledWaste / waste + recyclableWaste / waste;
+                timerText.text = "Time left: " + Mathf.Round(runTimer);
+            }
+
+                
+            timerTime -= Time.deltaTime;
+
+        }
+    }
+
+}
---
+++ Assets/SimulationManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEditor;
+using TMPro;
+
+public class SimulationManager : MonoBehaviour
+{
+    public SimulationAnimationManager simulationAnimationManager;
+    public float population;
+    public TMP_Text populationText;
+    public float years;
+    public TMP_Text timeText;
+    public float recyclingPercent;
+    public TMP_Text recyclingPercentText;
+    public float waste;
+    public float unrecycledWaste;
+    public float recycledWaste;
+    public float recyclableWaste;
+    
+
+    public void adjustPopulation(float pop)
+    {
+        population = (int)pop;
+        populationText.text = "Population : " + pop;
+    }
+
+    public void adjustTime(float yr)
+    {
+        years = (int)yr;
+        timeText.text = "Time(Years) : " + years;
+    }
+
+    public void adjustRecyclingPercentage(float prc)
+    {
+        recyclingPercent = Mathf.Round(prc) / 100;
+        recyclingPercentText.text = "People, who recycle : " + Mathf.Round(prc) + "%";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        population = 10000;
+        years = 1;
+        recyclingPercent = 0;
+
+    }
+
+
+
+    public void InitFormulas()
+    {
+        recycledWaste = Mathf.Round(years * ((0.407f * 0.31f) * (population * recyclingPercent)));
+        recyclableWaste = Mathf.Round(years * ((0.407f * 0.31f) * (population * (1 - recyclingPercent))));
+        unrecycledWaste = Mathf.Round(years * (0.407f * population) * (1-0.31f));
+        waste = unrecycledWaste + recycledWaste + recyclableWaste;
+        simulationAnimationManager.yearTime = simulationAnimationManager.maxRunTime / years;
+
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
